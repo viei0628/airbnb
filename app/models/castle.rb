223b4d4373ca5ab40c5611d8ebc9576d2eaf59d2
Timothy Vieira <vieira.timothy@yahoo.ca>
2021-01-31 07:05:09 -0500

    Added foreign key

--- app/models/castle.rb
+++ app/models/castle.rb
@@ -1,5 +1,5 @@
 class Castle < ApplicationRecord
-
+  belongs_to :user
   validates :name, :address, :photo, presence: true
   has_many :reviews, dependent: :destroy
   has_one_attached :photo, dependent: :destroy
